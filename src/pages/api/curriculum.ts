6005a59f1b4cc62e5428c944e2d10023b654565b
elraphty <elraphtyofficial@gmail.com>
2024-11-22 23:13:10 +0100

    feat: use openapi to categorize documents

--- src/pages/api/curriculum.ts
+++ src/pages/api/curriculum.ts
@@ -3,13 +3,14 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { handlePostRequest } from '@/lib/curriculumLogic';
 import { client } from '@/config/elasticsearch';
+import { generateCompletion } from '@/lib/openai';
 
 type ResponseData = {
     data: unknown;
     message: string;
 };
 
-const size = 70;
+const size = 50;
 const from = 0;
 
 const FIELDS_TO_SEARCH = ["title", "summary"];
@@ -60,10 +61,38 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
                 // select only the sources that has a summary property          
                 const sourcesWithSummary = sources.filter((item: any) => item.summary);
 
+                const sourcesLength = sourcesWithSummary.length;
+
+                // get only title and summary
+                const sourcesWithTitleAndSummary = sourcesWithSummary.map((item: any) => ({
+                    title: item.title,
+                    summary: item.summary,
+                }));
+
+                // divide the sources into 2 different arrays using the sourcesLength
+                const sourcesWithTitleAndSummaryArray1 = sourcesWithTitleAndSummary.slice(0, Math.floor(sourcesLength / 2));
+                const sourcesWithTitleAndSummaryArray2 = sourcesWithTitleAndSummary.slice(Math.floor(sourcesLength / 2), sourcesLength);
+
+                const completion1 = await generateCompletion(sourcesWithTitleAndSummaryArray1);
+                const completion2 = await generateCompletion(sourcesWithTitleAndSummaryArray2);
+
+                let combinedCompletion: any[] = [];
+
+                if (completion1) {
+                    // combine the 2 completions    
+                    combinedCompletion = [...combinedCompletion, ...completion1]
+                }
+
+                if (completion2) {
+                    // add the completion2 to the combinedCompletion
+                    combinedCompletion = [...combinedCompletion, ...completion2];
+                }
+
+
                 return res.status(200).json({
                     message: 'Success',
                     data: {
-                        result: sourcesWithSummary,
+                        result: combinedCompletion,
                     },
                 });
             } catch (error: any) {

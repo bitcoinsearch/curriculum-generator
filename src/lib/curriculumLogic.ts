7e56dff4ba76c9480b2f2d71006b8fba4026886f
elraphty <elraphtyofficial@gmail.com>
2024-11-22 12:49:26 +0100

    feat: Query ES

--- src/lib/curriculumLogic.ts
+++ src/lib/curriculumLogic.ts
@@ -5,12 +5,6 @@ export type MyData = {
     name: string;
 };
 
-export function fetchData(): MyData {
-    return {
-        id: 1,
-        name: 'Hello from the business logic of the curriculum!',
-    };
-}
 
 export function handlePostRequest(body: any): MyData {
     // Example processing logic for a POST request
--- src/pages/api/curriculum.ts
+++ src/pages/api/curriculum.ts
@@ -1,19 +1,70 @@
 // src/pages/api/myRoute.ts
 
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { fetchData, handlePostRequest } from '@/lib/curriculumLogic';
+import { handlePostRequest } from '@/lib/curriculumLogic';
+import { client } from '@/config/elasticsearch';
 
 type ResponseData = {
     data: unknown;
     message: string;
 };
 
-export default function handler(req: NextApiRequest, res: NextApiResponse<ResponseData>) {
+const size = 10;
+const from = 0;
+
+const FIELDS_TO_SEARCH = ["title", "summary"];
+
+
+let baseQuery = {
+    query: {
+        bool: {
+            must: [{
+                multi_match: {
+                    query: 'Segwit',
+                    fields: FIELDS_TO_SEARCH,
+                    fuzziness: 0,
+                    minimum_should_match: "90%",
+                },
+            }],
+            must_not: [
+                {
+                    term: {
+                        "type.keyword": "combined-summary",
+                    },
+                },
+            ],
+        },
+    },
+    size, // Number of search results to return
+    from, // Offset for pagination (calculated from page number)
+    _source: {
+        includes: ["title", "summary", "authors", "domain", "tags"],
+    },
+};
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse<ResponseData>) {
     switch (req.method) {
         case 'GET': {
-            const data = fetchData();
-            res.status(200).json({ message: 'Success', data });
-            break;
+            try {
+                // Call the search method
+                const result = await client.search({
+                    index: process.env.INDEX,
+                    ...baseQuery,
+                });
+
+                return res.status(200).json({
+                    message: 'Success',
+                    data: {
+                        result: result.hits.hits,
+                    },
+                });
+            } catch (error: any) {
+                console.error(error);
+                return res.status(400).json({
+                    message: error.errmsg || error.errors || error.message,
+                    data: null,
+                });
+            }
         }
         case 'POST': {
             const data = handlePostRequest(req.body);
